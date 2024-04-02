41f04586ea3ee40028f87aded6ab34291c8f0377
edu <vizuete.eduardo@gmail.com>
2024-04-02 16:00:11 -0500

    Section 7: Spring Boot REST API Validation

--- section_003_spring_boot_crud_mysql/springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
+++ section_003_spring_boot_crud_mysql/springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,19 @@ import lombok.Setter;
 public class UserDto {
 
     private Long id;
+
+    // user first name should not be null or empty
+    @NotEmpty(message = "User first name should not be null or empty")
     private String firstName;
+
+    // user last name should not be null or empty
+    @NotEmpty(message = "User last name should not be null or empty")
     private String lastName;
+
+    // user email sho       uld not be null or empty
+    // email address should be valid
+    @NotEmpty(message = "User email should not be null or empty")
+    @Email(message = "email address should be valid")
     private String email;
 
 }
