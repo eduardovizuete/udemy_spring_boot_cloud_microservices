6a65d444f9ded09499c70a2fa04b7832936bc5c1
edu <vizuete.eduardo@gmail.com>
2024-04-30 12:48:29 -0500

    Section 11: building microservices
    department rest api

---
+++ section_011_building_microservices/department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,53 @@
+package net.javaguides.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.departmentservice.dto.DepartmentDto;
+import net.javaguides.departmentservice.entity.Department;
+import net.javaguides.departmentservice.repository.DepartmentRepository;
+import net.javaguides.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        // convert department dto to department jpa entity
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department saveDepartment = departmentRepository.save(department);
+
+        // convert department jpa entity to department
+        DepartmentDto saveDepartmentDto = new DepartmentDto(
+                saveDepartment.getId(),
+                saveDepartment.getDepartmentName(),
+                saveDepartment.getDepartmentDescription(),
+                saveDepartment.getDepartmentCode()
+        );
+
+        return saveDepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String departmentCode) {
+        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+
+        // convert department jpa entity to department
+        DepartmentDto departmentDto = new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription(),
+                department.getDepartmentCode()
+        );
+
+        return departmentDto;
+    }
+}
